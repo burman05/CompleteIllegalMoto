e20dc540a7b213cc62f942c14b332387f296d697
OptimusPrimeRib <alex.s.burman@gmail.com>
2020-07-13 22:47:49 +0000

    its a secret v.v

---
+++ main.py
@@ -0,0 +1,17 @@
+
+print('today we will be learning list data structure in detail')
+numbers = [1,43,28383932,3,4,5,21,34]
+print('origional list of numbers', numbers)
+print("Last element of the the list is", numbers[-1])
+print("Range of elelments 2 to end", numbers[2:])
+
+print("Range of elelments starting with 0 to 3", 
+numbers[0:3])
+
+print("Maximum number in the List is", 
+max(numbers))
+print("Minimum number in the list is=",
+min(numbers))
+print("Sum of all numbers in the list", 
+sum(numbers))
+
